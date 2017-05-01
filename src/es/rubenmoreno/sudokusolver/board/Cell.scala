763f584616266c48b43a28e3fb913d8980feff4f
Ruben Moreno Montoliu <ruben3d@gmail.com>
2017-05-01 21:19:46 +0100

    Use dot for Empty cells instead of 'X'

--- src/es/rubenmoreno/sudokusolver/board/Cell.scala
+++ src/es/rubenmoreno/sudokusolver/board/Cell.scala
@@ -2,7 +2,7 @@ package es.rubenmoreno.sudokusolver.board
 
 sealed trait Cell
 case object Empty extends Cell {
-  override def toString: String = "X"
+  override def toString: String = "."
 }
 case class FreeCell(val value: Int) extends Cell {
   override def toString: String = value toString
